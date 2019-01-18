ab4c5d203ca22f1f3224e580d3bcce51e1ec8e59
mshsheng <mshsheng@126.com>
2019-01-18 17:51:42 +0800

    添加项目文件。

---
+++ CSL-AutoRocketLaunch/AutoRocketLaunchMod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+using UnityEngine;
+
+namespace CSL_AutoRocketLaunch
+{
+    public class AutoRocketLaunchMod : IUserMod
+    {
+        public string Name
+        {
+            get { return "Auto Rocket Launch"; }
+        }
+
+        public string Description
+        {
+            get { return "Launching rockets automatically"; }
+        }
+    }
+}
